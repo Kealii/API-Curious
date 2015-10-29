60ec82ab412faa9dc9029e171cea230685e6b534
Michael Wong <michael.brian.wong@gmail.com>
2015-10-29 00:03:28 -0600

    wip

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     @user = current_user
+    if @user
+    service = TwitterService.new(@user)
+    @tweets = service.tweets_for_user
+    end
   end
 end
--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -1,12 +1,17 @@
 require 'twitter'
 
 class TwitterService
-  def initialize
-    Twitter::REST::Client.new do |config|
+  attr_reader :client
+  def initialize(user)
+    @client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
       config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-      config.access_token_secret = ENV["TWITTER_TOKEN_SECRET"]
+      config.access_token        = user.oauth_token
+      config.access_token_secret = user.oauth_token_secret
     end
+  end
+
+  def tweets_for_user
+    @client.home_timeline
   end
 end
